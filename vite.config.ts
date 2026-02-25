849a57939b684b5598c0c300426f50c44da6969e
Rey <armentareyvincent59@gmail.com>
2026-02-25 10:13:11 +0800

    ci: allow legacy-peer-deps on install (Vercel)

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
 })
-
-=======
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  plugins: [vue()],
-})
-
->>>>>>> 48607c5 (Add calculator code)
